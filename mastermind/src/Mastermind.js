deae332dd4f5735c2ce3ae0c0a14639d576ec408
Binnur KURT <deepcloudlabs@gmail.com>
2025-11-04 21:39:42 +0300

    Update Mastermind.js

--- mastermind/src/Mastermind.js
+++ mastermind/src/Mastermind.js
@@ -4,26 +4,48 @@
 
  */
 import React from "react";
+import Container from "./components/common/container";
+import Card from "./components/common/card";
+import createSecret from "./utils/mastermind-util";
 
 class Mastermind extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-
+            level: 3,
+            secret: createSecret(3),
+            lives: 3,
+            timeout: 90,
+            maxMoves: 10,
+            counter: 90,
+            moves: [],
+            guess: 123
         }
     }
+
+
+    componentDidMount() {
+        this.timerId = setInterval(() => {
+            let nextState = {...this.state};
+            nextState.counter--;
+            console.log(this.state["counter"]);
+            this.setState(nextState, () => {
+                console.log(this.state["counter"]);
+            });
+        }, 5_000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
     render() {
         return ( // View
-            <div className="container">
-                <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Game Console</h3>
-                    </div>
-                    <div className="card-body">
-                        Mastermind
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Card title={"Game Console"}>
+                    {this.state["counter"]}
+                </Card>
+            </Container>
         );
     }
 }
