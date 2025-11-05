ce4a8a498240fde664e956cc0796d5d344151bda
Binnur KURT <deepcloudlabs@gmail.com>
2025-11-05 10:18:42 +0300

    Update mastermind-util.js

--- mastermind/src/utils/mastermind-util.js
+++ mastermind/src/utils/mastermind-util.js
@@ -1,4 +1,13 @@
 export default function createSecret(numberOfDigits=3) {
-    //TODO: implement secret generation logic
-    return 549;
+    const digits = [create_digit(1,9)];
+    while (digits.length < numberOfDigits) {
+        let digit = create_digit(0,9);
+        if (digits.includes(digit)) continue;
+        digits.push(digit);
+    }
+    return Number(digits.join(""));
+}
+
+function create_digit(min=0, max=9) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
